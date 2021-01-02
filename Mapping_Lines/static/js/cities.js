44e2a3420b57d0c61d30e38e7ad854477bc84568
unknown <phil.schechter@gmail.com>
2021-01-01 19:56:42 -0500

    mapping lines update

--- Mapping_Lines/static/js/cities.js
+++ Mapping_Lines/static/js/cities.js
@@ -31,3 +31,8 @@ let cities = [{
   }
   ];
 
+// Loop through the cities array and create one marker for each city.
+cities.forEach(function(city) {
+ console.log(city)
+ L.marker(city.location).addTo(map);
+});
